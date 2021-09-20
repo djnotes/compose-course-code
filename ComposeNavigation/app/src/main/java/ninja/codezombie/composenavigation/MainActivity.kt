e622b1c5817ec093c4f737f90aa476ad8dfa6726
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2021-09-20 02:12:40 -0400

    Prepared pre-record fruit selection

--- ComposeNavigation/app/src/main/java/ninja/codezombie/composenavigation/MainActivity.kt
+++ ComposeNavigation/app/src/main/java/ninja/codezombie/composenavigation/MainActivity.kt
@@ -1,18 +1,28 @@
 package ninja.codezombie.composenavigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.IndicationInstance
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHost
@@ -56,6 +66,10 @@ fun MyScreens() {
         composable("e_route"){
             E_Screen()
         }
+
+        //TODO: Add route to show image
+
+        //TODO: Add route to show greeting
     }
 }
 
@@ -82,11 +96,60 @@ fun HomeScreen(navController: NavHostController) {
             Text("Screen C")
         }
 
-        Text("Home Screen", style = MaterialTheme.typography.h1,
+        Text("Home Screen", style = MaterialTheme.typography.h6,
         modifier = Modifier
             .padding(8.dp)
             .align(Alignment.CenterHorizontally)
+
         )
+
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.LightGray, RoundedCornerShape(5.dp))
+            .clip(RoundedCornerShape(5.dp))
+        ) {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .selectableGroup()
+                .align(Alignment.CenterHorizontally)
+            ) {
+                val images = listOf(R.drawable.pineapple, R.drawable.apple, R.drawable.banana)
+                var selectedImage by remember{mutableStateOf(0)}
+                for(i in images.indices){
+                    val image = images[i]
+                    Image(painterResource(id = image), contentDescription = null, modifier = Modifier
+                        .size(100.dp)
+                        .padding(8.dp)
+                        .selectable(
+                            selected = selectedImage == i,
+                            onClick = {
+                                selectedImage = i
+                                Log.d(
+                                "MainActivity",
+                                "HomeScreen: Selected Image: $selectedImage. ")
+                            }
+                        )
+                        .background(if  (i == selectedImage) Color.Magenta else Color.Unspecified)
+                    )
+                }
+            }
+
+            val (slide, onSlideChange) = remember{ mutableStateOf(0f)}
+            Slider(value = slide , onValueChange = { onSlideChange(it);Log.d("MainActivity", "HomeScreen: slide: $slide") }, valueRange = 0f..3f, modifier = Modifier
+                .padding(horizontal = 8.dp))
+            Button(onClick = {
+                //TODO: Pass image and scale as arguments to the show route
+            }, modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(8.dp)
+            ) {
+                Text("Show Image")
+            }
+
+
+        }
+
+
     }
 }
 
@@ -161,6 +224,41 @@ fun E_Screen() {
     ){
         Text("Screen E", style = MaterialTheme.typography.h1,
         modifier = Modifier
+            .align(Alignment.Center)
+        )
+    }
+}
+
+/**
+ * Composable to show the result of user's selection
+ */
+@Composable
+fun ShowScreen(imageResource: Int, scale: Float) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)
+        .shadow(2.dp)
+    ) {
+       Image(painter = painterResource(imageResource), contentDescription = null, contentScale = ContentScale.Inside,
+       modifier = Modifier
+           .scale(scale)
+           .align(Alignment.Center)
+       )
+    }
+}
+
+/**
+ * Greet the user with the given name
+ */
+@Composable
+fun GreetingScreen(name: String) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)
+    ) {
+        Text("Welcome $name", style = MaterialTheme.typography.h1,
+        modifier = Modifier
+            .padding(8.dp)
             .align(Alignment.Center)
         )
     }
