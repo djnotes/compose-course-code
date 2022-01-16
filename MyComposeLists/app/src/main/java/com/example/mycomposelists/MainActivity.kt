dbfae48ce47efd162435fffb9c5b22b58f3ba094
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-01-16 05:40:02 -0500

    Added grid

--- MyComposeLists/app/src/main/java/com/example/mycomposelists/MainActivity.kt
+++ MyComposeLists/app/src/main/java/com/example/mycomposelists/MainActivity.kt
@@ -4,24 +4,25 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -69,12 +70,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(Modifier
-                        .fillMaxSize()
-                    ) {
-                        MyHorizontalList()
-                        MyVerticalListUi()
-                    }
+//                    Column(Modifier
+//                        .fillMaxSize()
+//                    ) {
+//                        MyHorizontalList()
+//                        MyVerticalListUi()
+//                    }
+                    MyGrid()
                 }
             }
         }
@@ -174,4 +176,43 @@ fun MyHorizontalList() {
         }
     }
 
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MyGrid() {
+    var selectedItem by remember{mutableStateOf("")}
+    LazyVerticalGrid(cells = GridCells.Adaptive(100.dp), modifier = Modifier
+        .background(Color(0xFFF06292))){
+        ProfilesList.forEach { profile->
+            item {
+                key(profile.id){
+                    Image(painter = painterResource(id = profile.picture),
+                        contentDescription = profile.name,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .width(100.dp)
+                            .height(150.dp)
+                            .scale(
+                                animateFloatAsState(if (selectedItem == profile.id) 1.5f else 1.0f,
+                                animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)
+                                )
+                                    .value
+                            )
+                            .background(Color.White)
+                            .padding(2.dp)
+                            .clickable {
+                                selectedItem = profile.id
+                            }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMyGrid() {
+    MyGrid()
 }
